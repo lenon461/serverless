6e0fe2b99e9092336eb670c260f5c951417b2efe
ljs <lenon461@gmail.com>
2020-01-12 01:20:44 +0900

    :tada: first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+exports.helloWorld = function (req, res) {
+    res.send("Hello, World2");
+};
---
+++ index.ts
@@ -0,0 +1,4 @@
+
+export const helloWorld = (req, res) => {
+  res.send("Hello, World2");
+};
